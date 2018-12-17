e2b2b1f46418fdfb5e592b1b1f0e48efce5438d0
Mason <mason@myclientglobal.com>
2018-12-18 11:08:15 +1300

    vendor: commit vendor directory

---
+++ vendor/cometbackup/comet-php-sdk/Comet/AdminMetaSoftwareUpdateNewsRequest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Copyright (c) 2018 Comet Licensing Ltd.
+ * Please see the LICENSE file for usage information.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Comet;
+
+/** 
+ * Comet Server AdminMetaSoftwareUpdateNews API 
+ * Get software update news from the software provider
+ * 
+ * You must supply administrator authentication credentials to use this API.
+ */
+class AdminMetaSoftwareUpdateNewsRequest implements \Comet\NetworkRequest {
+	
+	/**
+	 * Construct a new AdminMetaSoftwareUpdateNewsRequest instance.
+	 *
+	 */
+	public function __construct()
+	{
+	}
+	
+	/**
+	 * Get the URL where this POST request should be submitted to.
+	 *
+	 * @return string
+	 */
+	public function Endpoint()
+	{
+		return '/api/v1/admin/meta/software-update-news';
+	}
+	
+	/**
+	 * Get the POST parameters for this request.
+	 *
+	 * @return string[]
+	 */
+	public function Parameters()
+	{
+		$ret = [];
+		return $ret;
+	}
+	
+	/**
+	 * Decode types used in a response to this request.
+	 * Use any network library to make the request.
+	 *
+	 * @param int $responseCode HTTP response code
+	 * @param string $body HTTP response body
+	 * @return \Comet\SoftwareUpdateNewsResponse 
+	 * @throws \Exception
+	 */
+	public static function ProcessResponse($responseCode, $body)
+	{
+		// Require expected HTTP 200 response
+		if ($responseCode !== 200) {
+			throw new \Exception("Unexpected HTTP " . intval($responseCode) . " response");
+		}
+		
+		// Decode JSON
+		$decoded = \json_decode($body); // as stdClass
+		if (\json_last_error() != \JSON_ERROR_NONE) {
+			throw new \Exception("JSON decode failed: " . \json_last_error_msg());
+		}
+		
+		// Try to parse as error format
+		$isCARMDerivedType = (($decoded instanceof \stdClass) && property_exists($decoded, 'Status') && property_exists($decoded, 'Message'));
+		if ($isCARMDerivedType) {
+			$carm = \Comet\APIResponseMessage::createFromStdclass($decoded);
+			if ($carm->Status !== 200) {
+				throw new \Exception("Error " . $carm->Status . ": " . $carm->Message);
+			}
+		}
+		
+		// Parse as SoftwareUpdateNewsResponse
+		$ret = \Comet\SoftwareUpdateNewsResponse::createFromStdclass($decoded);
+		
+		return $ret;
+	}
+	
+}
+
